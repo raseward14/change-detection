739351c0714460b57ebe1a716b3c110126e46e7c
raseward14 <raseward14@gmail.com>
2026-02-25 18:12:11 -0700

    removed the ChangeDetectorRef and messages array from Messages List component in prep for the async pipe

--- src/app/messages/messages-list/messages-list.component.ts
+++ src/app/messages/messages-list/messages-list.component.ts
@@ -2,8 +2,6 @@ import {
   Component, 
   ChangeDetectionStrategy, 
   inject, 
-  ChangeDetectorRef, 
-  OnInit, 
   DestroyRef
 } from '@angular/core';
 
@@ -16,22 +14,9 @@ import { MessagesService } from '../messages.service';
   styleUrl: './messages-list.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class MessagesListComponent implements OnInit {
+export class MessagesListComponent {
   private messagesService = inject(MessagesService);
-  private cdRef = inject(ChangeDetectorRef);
   private destroyRef = inject(DestroyRef);
-
-  messages: string[] = [];
-
-  ngOnInit() {
-    const subscription = this.messagesService.messages$.subscribe((messages) => {
-      this.messages = messages;
-      this.cdRef.markForCheck();
-    });
-    this.destroyRef.onDestroy(() => {
-      subscription.unsubscribe();
-    });
-  }
 
   get debugOutput() {
     console.log('[MessagesList] "debugOutput" binding re-evaluated.');
